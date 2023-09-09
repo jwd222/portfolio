03b9e7e65535e29aec6c92377c650c0adb6d9b2d
jwd222 <sji.2.sami@gmail.com>
2023-09-09 14:51:26 +0600

    made bg a little darker

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className}bg-gray-50 
+        className={`${inter.className} bg-gray-100
         text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         <div
