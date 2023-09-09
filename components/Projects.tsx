9871cc712a4504750ad2d97458238052169501bb
jwd222 <sji.2.sami@gmail.com>
2023-09-10 03:56:54 +0600

    made custom hook to set the active section name and refactored code

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,16 @@
+'use client'
+
 import React from 'react'
 import { projectsData } from '@/lib/data'
 import SectionHeading from './SectionHeading'
 import Project from './Project'
+import { useSectionInView } from '@/lib/hooks'
+
 const Projects = () => {
+  const { ref } = useSectionInView('Projects', 0.5)
+
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section id="projects" className="scroll-mt-28" ref={ref}>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
