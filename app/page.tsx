4e4454fa9b07314b9a196550ee376ef5410df6f1
jwd222 <sji.2.sami@gmail.com>
2023-09-09 13:27:13 +0600

    navbar styling and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="">
-      <h2>Hello</h2>
-    </main>
-  )
+  return <main className=""></main>
 }
---
+++ lib/data.ts
@@ -0,0 +1,99 @@
+import React from 'react'
+import { CgWorkAlt } from 'react-icons/cg'
+import { FaReact } from 'react-icons/fa'
+import { LuGraduationCap } from 'react-icons/lu'
+import colorpickerImg from '@/public/color-picker.png'
+import ghuraghuriImg from '@/public/ghuraghuri.png'
+import jobifyImg from '@/public/jobify.png'
+import youtubecloneImg from '@/public/youtube-clone.png'
+
+export const links = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Projects',
+    hash: '#projects',
+  },
+  {
+    name: 'Skills',
+    hash: '#skills',
+  },
+  {
+    name: 'Experience',
+    hash: '#experience',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const
+
+export const experiencesData = [
+  {
+    title: 'Graduated bootcamp',
+    location: 'Miami, FL',
+    description:
+      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+    icon: React.createElement(LuGraduationCap),
+    date: '2019',
+  },
+  {
+    title: 'Front-End Developer',
+    location: 'Orlando, FL',
+    description:
+      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+    icon: React.createElement(CgWorkAlt),
+    date: '2019 - 2021',
+  },
+  {
+    title: 'Full-Stack Developer',
+    location: 'Houston, TX',
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: '2021 - present',
+  },
+] as const
+
+export const projectsData = [
+  {
+    title: 'Jobify',
+    description: 'description for jobiify',
+    tags: ['MongoDB', 'Express', 'React', 'Node.js'],
+    imageUrl: jobifyImg,
+  },
+  {
+    title: 'YouTube Clone',
+    description: 'description for YouTube clone',
+    tags: ['React'],
+    imageUrl: youtubecloneImg,
+  },
+] as const
+
+export const skillsData = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Next.js',
+  'Node.js',
+  'Git',
+  'Tailwind',
+  'Prisma',
+  'MongoDB',
+  'Redux',
+  'GraphQL',
+  'Apollo',
+  'Express',
+  'PostgreSQL',
+  'Python',
+  'Django',
+  'Framer Motion',
+] as const
