63110d1b73a8805e823ec73acbd44efb2cbd4ba4
jwd222 <sji.2.sami@gmail.com>
2023-09-10 16:07:00 +0600

    experience section completed

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,15 +1,48 @@
+'use client'
+
+import { experiencesData } from '@/lib/data'
 import SectionHeading from './SectionHeading'
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import React from 'react'
 
 const Experience = () => {
   return (
     <section id="experience">
       <SectionHeading>My experience</SectionHeading>
-      <VerticalTimeline></VerticalTimeline>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: '#f3f4f66',
+                boxShadow: 'none',
+                border: 'solid rgba(0,0,0, 0.06)',
+                textAlign: 'left',
+                padding: '1.3rem 2rem',
+              }}
+              contentArrowStyle={{
+                borderRight: '0.5rem solid #9ca3af',
+              }}
+              icon={item.icon}
+              iconStyle={{
+                background: 'white',
+                fontSize: '1rem',
+              }}
+              date={item.date}
+            >
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">
+                {item.description}
+              </p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   )
 }
