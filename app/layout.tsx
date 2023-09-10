d8043f5662c4a098990a41711d67ec252621f762
jwd222 <sji.2.sami@gmail.com>
2023-09-10 23:48:01 +0600

    dark mode dom class added and toggle functional

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ActiveSectionContextProvider } from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
 import Footer from '@/components/Footer'
 import ThemeSwitch from '@/components/ThemeSwitch'
+import { ThemeContextProvider } from '@/context/theme-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="en" className="!scroll-smooth">
       <body
         className={`${inter.className} bg-gray-100
-        text-gray-950 relative pt-28 sm:pt-36`}
+        text-gray-950 relative pt-28 sm:pt-36 
+        dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div
           className="bg-[#fbe2e3] absolute top-[-6rem] -z-10
@@ -28,17 +30,20 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <div
           className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 
         left-[-35rem] h-[31.25rem] w-[50rem] 
-        rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-{-28rem]
-          xl:left-[-15rem] 2xl:left-[-5rem]"
+        rounded-full blur-[10rem] sm:w-[68.75rem] 
+        md:left-[-33rem] lg:left-{-28rem] xl:left-[-15rem] 
+          2xl:left-[-5rem]"
         ></div>
-        <ActiveSectionContextProvider>
-          <Header />
-          {children}
-          <Footer />
-          <Toaster position="top-right" />
-        </ActiveSectionContextProvider>
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header />
+            {children}
+            <Footer />
 
-        <ThemeSwitch />
+            <Toaster position="top-right" />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,4 +16,5 @@ module.exports = {
   },
   plugins: [],
   mode: 'jit',
+  darkMode: 'class',
 }
