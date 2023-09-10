735cadcb1e65f69819d584372bc31cfb923bc2f8
jwd222 <sji.2.sami@gmail.com>
2023-09-10 20:10:32 +0600

    helper functions

---
+++ lib/utils.ts
@@ -0,0 +1,24 @@
+const validateString = (value: unknown, maxLength: number) => {
+  if (!value || typeof value !== 'string' || value.length > maxLength) {
+    return false
+  }
+
+  return true
+}
+
+const getErrorMessage = (error: unknown): string => {
+  let message: string
+  if (error instanceof Error) {
+    message = String(error.message)
+  } else if (error && typeof error === 'object' && 'message' in error) {
+    message = String(error.message)
+  } else if (typeof error === 'string') {
+    message = error
+  } else {
+    message = 'Something went wrong.'
+  }
+
+  return message
+}
+
+export { validateString, getErrorMessage }
