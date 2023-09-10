5b7d41b993440225f273f12cb045a31dc6ee41f0
jwd222 <sji.2.sami@gmail.com>
2023-09-10 16:16:08 +0600

    contact start

---
+++ components/Contact.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from './SectionHeading'
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <SectionHeading>Contact me</SectionHeading>
+      <p>Please contact me directly at shahjawad@iut-dhaka.</p>
+    </section>
+  )
+}
+export default Contact
