bdecf6cf8c9b454bc1e2cb001033815cc2a42fb2
jwd222 <sji.2.sami@gmail.com>
2023-09-12 04:58:47 +0600

    added new breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      xs: { max: '640px' },
+      sm: { min: '640px' },
+    },
   },
   plugins: [],
   mode: 'jit',
